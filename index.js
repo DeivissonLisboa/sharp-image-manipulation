759d0955034409330a83975992c2215347d0c2d9
Deivisson Lisboa <dvilaflor@gmail.com>
2023-06-29 10:58:55 -0300

    Add colors hex

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const width = 300,
   height = 300,
   background = "#ff0000"
 
+const colors = [
+  "#eddcd2",
+  "#fff1e6",
+  "#fde2e4",
+  "#fad2e1",
+  "#c5dedd",
+  "#dbe7e4",
+  "#f0efeb",
+  "#d6e2e9",
+  "#bcd4e6",
+  "#99c1de",
+]
+
 const sprite_path = __dirname + "/assets/sprites/tile000.png",
   sprite_width = width / 2,
   sprite_height = height / 2
